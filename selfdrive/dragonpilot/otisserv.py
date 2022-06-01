e4522f204d337c2f1ce3448b7e64432407c4e268
Rick Lan <ricklan@gmail.com>
2022-06-01 21:29:35 +0800

    fix CI breaks otisserv

--- selfdrive/dragonpilot/otisserv.py
+++ selfdrive/dragonpilot/otisserv.py
@@ -23,7 +23,7 @@
 # THE SOFTWARE.
 
 from http.server import BaseHTTPRequestHandler, HTTPServer
-#from cgi import parse_header, parse_multipart
+from cgi import parse_header, parse_multipart
 from urllib.parse import parse_qs, unquote
 import json
 import requests
