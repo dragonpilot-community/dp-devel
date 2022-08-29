478bf0e738de1f0e1343f696c21a7fc2f198c9de
Rick Lan <ricklan@gmail.com>
2022-08-29 15:13:05 +0800

    ability to turn off mapd service

--- selfdrive/manager/manager.py
+++ selfdrive/manager/manager.py
@@ -137,7 +137,9 @@ def manager_thread() -> None:
   dp_otisserv = params.get_bool('dp_otisserv')
   ignore += ['dmonitoringmodeld', 'dmonitoringd'] if params.get_bool('dp_jetson') else []
   ignore += ['otisserv'] if not dp_otisserv else []
-  ignore += ['gpxd'] if not dp_otisserv and not params.get_bool('dp_gpxd') else []
+  dp_mapd = params.get_bool('dp_mapd')
+  ignore += ['mapd'] if not dp_mapd else []
+  ignore += ['gpxd'] if not dp_otisserv and not dp_mapd and not params.get_bool('dp_gpxd') else []
   ignore += ['uploader'] if not params.get_bool('dp_api_custom') and (int(params.get('dp_atl', encoding='utf8')) > 0 or params.get_bool('dp_jetson')) else []
 
   if params.get("DongleId", encoding='utf8') in (None, UNREGISTERED_DONGLE_ID):
