7e1eec705ce5d9b41288c5f1c8d66cd92bae81db
Rick Lan <ricklan@gmail.com>
2022-08-30 15:53:55 +0800

    remove CJK fonts

--- selfdrive/ui/qt/window.cc
+++ selfdrive/ui/qt/window.cc
@@ -45,9 +45,6 @@ MainWindow::MainWindow(QWidget *parent) : QWidget(parent) {
   });
 
   // load fonts
-  QFontDatabase::addApplicationFont("../assets/fonts/NotoSansCJKtc-Regular.otf");
-  QFontDatabase::addApplicationFont("../assets/fonts/NotoSansCJKtc-Medium.otf");
-  QFontDatabase::addApplicationFont("../assets/fonts/NotoSansCJKtc-Bold.otf");
   QFontDatabase::addApplicationFont("../assets/fonts/Inter-Black.ttf");
   QFontDatabase::addApplicationFont("../assets/fonts/Inter-Bold.ttf");
   QFontDatabase::addApplicationFont("../assets/fonts/Inter-ExtraBold.ttf");
