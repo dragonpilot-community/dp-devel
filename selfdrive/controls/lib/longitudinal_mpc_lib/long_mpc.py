467f717fe692fb83f9072a8a58d84b56b9aa9fb3
AlexandreSato <66435071+AlexandreSato@users.noreply.github.com>
2022-12-15 16:35:33 -0300

    increase tha max limit of v_diff_offset
    Allows the car to exit faster when the lead comes back in motion, we've been using it since May 2nd: <https://github.com/AlexandreSato/animalpilot/commit/081ccd2a3ac7f3aa41d6a75a1b27fdab0410a108>

--- selfdrive/controls/lib/longitudinal_mpc_lib/long_mpc.py
+++ selfdrive/controls/lib/longitudinal_mpc_lib/long_mpc.py
@@ -63,7 +63,7 @@ def get_stopped_equivalence_factor(v_lead, v_ego):
   v_diff_offset = 0
   if np.all(v_lead - v_ego > 0):
     v_diff_offset = ((v_lead - v_ego) * 1.)
-    v_diff_offset = np.clip(v_diff_offset, 0, STOP_DISTANCE / 2)
+    v_diff_offset = np.clip(v_diff_offset, 0, STOP_DISTANCE)
     v_diff_offset = np.maximum(v_diff_offset * ((10 - v_ego)/10), 0)
   distance = (v_lead**2) / (2 * COMFORT_BRAKE) + v_diff_offset
   return distance
