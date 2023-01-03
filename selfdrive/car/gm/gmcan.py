82e5de90cdbc5d053781b762e3d76dd96037e520
rav4kumar <36933347+rav4kumar@users.noreply.github.com>
2023-01-03 13:21:38 -0700

    trailblazer support

--- selfdrive/car/gm/gmcan.py
+++ selfdrive/car/gm/gmcan.py
@@ -66,7 +66,7 @@ def create_friction_brake_command(packer, bus, apply_brake, idx, enabled, near_s
   mode = 0x1
 
   # TODO: Understand this better. Volts and ICE Camera ACC cars are 0x1 when enabled with no brake
-  if enabled and CP.carFingerprint in (CAR.BOLT_EUV,):
+  if enabled and CP.carFingerprint in (CAR.BOLT_EUV, CAR.TRAILBLAZER): # FIXME: Add trailblazer in here. It is not certain. In this case, there is no problem.
     mode = 0x9
 
   if apply_brake > 0:
