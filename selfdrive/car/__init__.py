02d4ccf72dcbd0c48527b8a1d42494378d3a7f48
Bellri <RedFox102@gmail.com>
2023-02-16 21:56:24 -0500

    Update __init__.py

--- selfdrive/car/__init__.py
+++ selfdrive/car/__init__.py
@@ -70,26 +70,6 @@ def scale_tire_stiffness(mass, wheelbase, center_to_front, tire_stiffness_factor
 def dbc_dict(pt_dbc, radar_dbc, chassis_dbc=None, body_dbc=None) -> Dict[str, str]:
   return {'pt': pt_dbc, 'radar': radar_dbc, 'chassis': chassis_dbc, 'body': body_dbc}
 
-#alternate settings when using torque interceptor. May or may not be useful to some users/branches.
-def apply_ti_steer_torque_limits(apply_torque, apply_torque_last, driver_torque, LIMITS):
-
-  # limits due to driver torque
-  driver_max_torque = LIMITS.TI_STEER_MAX + (LIMITS.TI_STEER_DRIVER_ALLOWANCE + driver_torque * LIMITS.TI_STEER_DRIVER_FACTOR) * LIMITS.TI_STEER_DRIVER_MULTIPLIER
-  driver_min_torque = -LIMITS.TI_STEER_MAX + (-LIMITS.TI_STEER_DRIVER_ALLOWANCE + driver_torque * LIMITS.TI_STEER_DRIVER_FACTOR) * LIMITS.TI_STEER_DRIVER_MULTIPLIER
-  max_steer_allowed = max(min(LIMITS.TI_STEER_MAX, driver_max_torque), 0)
-  min_steer_allowed = min(max(-LIMITS.TI_STEER_MAX, driver_min_torque), 0)
-  apply_torque = clip(apply_torque, min_steer_allowed, max_steer_allowed)
-
-  # slow rate if steer torque increases in magnitude
-  if apply_torque_last > 0:
-    apply_torque = clip(apply_torque, max(apply_torque_last - LIMITS.TI_STEER_DELTA_DOWN, -LIMITS.TI_STEER_DELTA_UP),
-                        apply_torque_last + LIMITS.TI_STEER_DELTA_UP)
-  else:
-    apply_torque = clip(apply_torque, apply_torque_last - LIMITS.TI_STEER_DELTA_UP,
-                        min(apply_torque_last + LIMITS.TI_STEER_DELTA_DOWN, LIMITS.TI_STEER_DELTA_UP))
-
-  return int(round(float(apply_torque)))  
-
 
 def apply_std_steer_torque_limits(apply_torque, apply_torque_last, driver_torque, LIMITS):
 
