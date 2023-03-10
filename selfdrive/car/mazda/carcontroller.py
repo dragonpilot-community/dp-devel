330ea69f35805d0467281a10231e800834a5221a
Bellri <RedFox102@gmail.com>
2023-03-10 00:06:32 -0500

    Comment

--- selfdrive/car/mazda/carcontroller.py
+++ selfdrive/car/mazda/carcontroller.py
@@ -72,7 +72,7 @@ class CarController:
     #The ti cannot be detected unless OP sends a can message to it becasue the ti only transmits when it
     #sees the signature key in the designated address range.
     can_sends.append(mazdacan.create_ti_steering_control(self.packer, CS.CP.carFingerprint, self.frame, ti_apply_steer))
-    # send steering command
+    # Always send steering command to stock system, even with TI enabled.
     can_sends.append(mazdacan.create_steering_control(self.packer, self.CP.carFingerprint,
                                                       self.frame, apply_steer, CS.cam_lkas))
 
